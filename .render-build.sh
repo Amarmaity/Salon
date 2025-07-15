876bc7aac6cb04400f572ae2d319b3eea97051db
Amar <amar.maity@delostylestudio.com>
2025-07-15 10:58:23 +0530

    Add render build script for React and Django

--- .render-build.sh
+++ .render-build.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
+
+# Exit on any error
+set -o errexit
+
+# Build React frontend
+cd frontend
+npm install
+npm run build
+
+# Go back to root
+cd ..
+
+# Collect static files
 python manage.py collectstatic --noinput
